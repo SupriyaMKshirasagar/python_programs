0dcd685b21d63577503d5198f15f1e8bd12d3725
SupriyaMKshirasagar <supriyajuly162001@gmail.com>
2023-04-15 15:41:22 +0530

    change

--- programs/assignmentonEMI.py
+++ programs/assignmentonEMI.py
@@ -1,3 +1,4 @@
+from tkinter import*
 import tkinter as Tk
 
 
@@ -14,18 +15,24 @@ def calculate():
     total_payment.set('{:.2f}'.format(total_payment_val))
     
     pass
+ 
+
+
 
 main_window=Tk.Tk()
 main_window.title=("EMI calculator using python")
 main_window.geometry("")
+main_window.configure(bg="#f0c33c")
 
+Tk.Label(main_window,text='EMI CALCULATOR',font=('impact',20),bg="#f0c33c",padx=10,pady=10).grid(row=0)
 #Creating label
-Tk.Label(main_window,text='Amount',padx=10,pady=10).grid(row=0)
-Tk.Label(main_window,text='Interest',padx=10,pady=10).grid(row=1)
-Tk.Label(main_window,text='Duration',padx=10,pady=10).grid(row=2)
+Tk.Label(main_window,text='Amount',bg="#f0c33c",padx=10,pady=10).grid(row=1)
+Tk.Label(main_window,text='Interest',bg="#f0c33c",padx=10,pady=10).grid(row=2)
+Tk.Label(main_window,text='Duration',bg="#f0c33c",padx=10,pady=10).grid(row=3)
 
 #Creating Buttons
-Tk.Button(main_window,text="Calculate",command=calculate,padx=10,pady=10).grid(row=3,columnspan=2)
+Tk.Button(main_window,text="Calculate",command=calculate,bg="#f0c33c",padx=10,pady=10).grid(row=4,columnspan=1)
+
 
 
 
@@ -43,21 +50,18 @@ total_interest.set(0)
 total_payment.set(0)
 
 
-Tk.Entry(main_window,textvariable=principal).grid(row=0,column=1)
-Tk.Entry(main_window,textvariable=rate).grid(row=1,column=1)
-Tk.Entry(main_window,textvariable=years).grid(row=2,column=1)
+Tk.Entry(main_window,textvariable=principal).grid(row=1,column=1)
+Tk.Entry(main_window,textvariable=rate).grid(row=2,column=1)
+Tk.Entry(main_window,textvariable=years).grid(row=3,column=1)
 
-Tk.Label(main_window,text='EMI',padx=10,pady=10).grid(row=4,columnspan=2)
-Tk.Label(main_window,textvariable=emi,font=('Arial',16),padx=10,pady=10).grid(row=5,columnspan=2)
-Tk.Label(main_window,text='Total Interest',padx=10,pady=10).grid(row=6,columnspan=2)
-Tk.Label(main_window,textvariable=total_interest,font=('Arial',16),padx=10,pady=10).grid(row=7,columnspan=2)
-Tk.Label(main_window,text='Total Amount',padx=10,pady=10).grid(row=8,columnspan=2)
-Tk.Label(main_window,textvariable=total_payment,font=('Arial',16),padx=10,pady=10).grid(row=9,columnspan=2)
+Tk.Label(main_window,text='EMI',bg="#f0c33c",padx=10,pady=10).grid(row=5,columnspan=2)
+Tk.Label(main_window,textvariable=emi,font=('Arial',16),bg="#f0c33c",padx=10,pady=10).grid(row=6,columnspan=2)
+Tk.Label(main_window,text='Total Interest',bg="#f0c33c",padx=10,pady=10).grid(row=7,columnspan=2)
+Tk.Label(main_window,textvariable=total_interest,font=('Arial',16),bg="#f0c33c",padx=10,pady=10).grid(row=8,columnspan=2)
+Tk.Label(main_window,text='Total Amount',bg="#f0c33c",padx=10,pady=10).grid(row=9,columnspan=2)
+Tk.Label(main_window,textvariable=total_payment,font=('Arial',16),bg="#f0c33c",padx=10,pady=10).grid(row=10,columnspan=2)
 
-
-
-
-
+    
 
 
 
